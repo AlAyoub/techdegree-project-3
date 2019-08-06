a28523c2c175a880e5793958d1511b29dcb50768
Alan Ayoub <alanayoub@me.com>
2019-08-06 14:04:01 -0700

    project completed

--- js/app.js
+++ js/app.js
@@ -2,103 +2,193 @@
 // Treehouse Techdegree
 // FSJS Project 3 - Interactive Form
 
-// On page load, the name input field is active
-// If job role other is selected, show other job role text field
-$(document).ready(function() {
-    $("#name").focus();
-    $('#otherjob').hide(); 
 
+
+$(document).ready(function () {
+
+  // mouse focus in the name input and hide other job input field
+  $("#name").focus();
+  $('#otherjob').hide();
+
+  // shows the other input field if other is selected from the job role select drop dowm
   $('select[name="user_title"]').change(function () {
     if ($(this).val() == "other") {
       $('#otherjob').show();
     } else {
       $('#otherjob').hide();
     }
-});
+  });
 
-// User Story #4
-// Hide all color options first
-// Then display the default text 'Please select a T-shirt theme'
-$('#color').hide();
-// $('#color option').text('Please select a T-shirt theme');
+  // hide all color options
+  $('#colors-js-puns label').hide();
+  $('#color').hide();
 
+  // default text when no t-shirt theme has been selected, initially hidden
+  const $defaultText = $("#color option:contains('Please select a T-shirt theme')").hide();
 
-// User Story #5
-// If Theme JS Puns is selected - hide last 3 options and show first 3 options
-// Else If Theme I Love JS is selected - hide first 3 options andshow last 3 options
-
-$('select[name="user_design"]').change(function () {
-  if ($(this).val() == "default") {
-    $('#color option').text('Please select a T-shirt theme');
-  } else if ($(this).val() == "js puns") {
-
-    $('#color').show();
-    $("#color option:contains('Cornflower Blue (JS Puns shirt only)')").prop('selected', true);
-    $("#color option:contains('I ♥ JS shirt only')").hide();
-    $("#color option:contains('JS Puns shirt only')").show();
-  } else if ($(this).val() == "heart js") {
+  // only show t-shirt options for the design selected
+  $('select[name="user_design"]').change(function () {
+    if ($(this).val() == "js puns") {
+      $('#colors-js-puns label').show();
+      $('#color').show();
+      $("#color option:contains('Cornflower Blue (JS Puns shirt only)')").prop('selected', true);
+      $("#color option:contains('I ♥ JS shirt only')").hide();
+      $("#color option:contains('JS Puns shirt only')").show();
+    } else if ($(this).val() == "heart js") {
+      $('#colors-js-puns label').show();
       $('#color').show();
       $("#color option:contains('Tomato (I ♥ JS shirt only)')").prop('selected', true);
       $("#color option:contains('JS Puns shirt only')").hide();
       $("#color option:contains('I ♥ JS shirt only')").show();
-    } 
+    } else {
+      $('#colors-js-puns label').show();
+      $("#color option:contains('Please select a T-shirt theme')").prop('selected', true);
+      $("#color option:contains('JS Puns shirt only')").hide();
+      $("#color option:contains('I ♥ JS shirt only')").hide();
+    }
+  });
+
+
+  // users cannot select two events that occur at the same time
+  $('.activities :checkbox').change(function () {
+    if (this.checked && this.name === "all") {
+    }
+    else if (this.checked && this.name === "build-tools") {
+    }
+    else if (this.checked && this.name === "npm") {
+    }
+    else if (this.checked && this.name === "js-frameworks") {
+      $('input[name="express"]').prop("disabled", true);
+      $('input[name="express"]').parent().css('color', 'grey');
+    }
+    else if (this.checked && this.name === "express") {
+      $('input[name="js-frameworks"]').prop("disabled", true);
+      $('input[name="js-frameworks"]').parent().css('color', 'grey');
+    }
+    else if (this.checked && this.name === "js-libs") {
+      $('input[name="node"]').prop("disabled", true);
+      $('input[name="node"]').parent().css('color', 'grey');
+    }
+    else if (this.checked && this.name === "node") {
+      $('input[name="js-libs"]').prop("disabled", true);
+      $('input[name="js-libs"]').parent().css('color', 'grey');
+    }
+    else {
+      $('input[name="express"]').prop("disabled", false);
+      $('input[name="js-frameworks"]').prop("disabled", false);
+      $('input[name="node"]').prop("disabled", false);
+      $('input[name="js-libs"]').prop("disabled", false);
+      $('input[name="express"]').parent().css('color', 'black');
+      $('input[name="js-frameworks"]').parent().css('color', 'black');
+      $('input[name="node"]').parent().css('color', 'black');
+      $('input[name="js-libs"]').parent().css('color', 'black');
+    }
+  });
+
+  // cost added to the register for activities total if checkbox is checked
+  $('label').click(function () {
+    var total = 0;
+    $('.activities :checked').each(function () {
+      total += parseInt($(this).val());
+    })
+    $('#total').html('Total $' + total);
+  });
+
+  // make credit card option the default selection and hides the paypal and botcoin text
+  $("#payment option:contains('Credit Card')").prop('selected', true);
+  $('#paypal').hide();
+  $('#bitcoin').hide();
+
+  // display the selelcted payment option
+  $('#payment').change(function () {
+    if ($('#payment option:selected').val() === "paypal") {
+      $('#paypal').show();
+      $('#bitcoin').hide();
+      $('#credit-card').hide();
+    } else if ($('#payment option:selected').val() === "bitcoin") {
+      $('#bitcoin').show();
+      $('#paypal').hide();
+      $('#credit-card').hide();
+    } else {
+      $('#credit-card').show();
+      $('#bitcoin').hide();
+    }
+  });
+
+
+  /////////////////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////////////
+
+  //                        FORM VALIDATION                                 //
+
+  /////////////////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////////////
+
+
+
+  // all regex variables are listed here
+  regexName = /^\s*$/;
+  regexEmail = /^\S+@\S+\.\S+$/;
+  regexCreditCard = /^(?:(4[0-9]{12}(?:[0-9]{3})?)|(5[1-5][0-9]{14})|(6(?:011|5[0-9]{2})[0-9]{12})|(3[47][0-9]{13})|(3(?:0[0-5]|[68][0-9])[0-9]{11})|((?:2131|1800|35[0-9]{3})[0-9]{11}))$/;
+  regexZip = /^\d{5}$/;
+  regexCvv = /^\d{3}$/;
+  var error = "";
+
+  // add a div with the id #error for text to display at the top if there is an error and initially hide the div
+  $('form').prepend('<div id="error"></div>');
+  $('#error').hide();
+
+
+  // handles our form submission
+  $('form').submit(function (e) {
+    
+    // prevents the default refresh of the submit button
+    e.preventDefault();
+
+    // regex validation for the name, email, activities, credit card, zip, and cvv fields
+    if (regexName.test($('#name').val())) {
+      console.log("The name field is blank");
+      error = "<h3>The name field cannot be left blank. Please enter your name.<br></h3>";
+      $('#error').css('color', 'red').show();
+    }
+    else if (!regexEmail.test($('#mail').val())) {
+      console.log("The email field is not in the correct format");
+      error = "<h3>The email address is not in the correct format. Please enter a valid email address.<br></h3>";
+      $('#error').css('color', 'red').show();
+    }
+    else if ($(".activities > label > input:checked").length === 0) {
+      console.log("No checkboxes selected.");
+      error = "<h3>You have not selected an activity. Please select an activity.<br></h3>";
+      $('#error').css('color', 'red').show();
+    }
+    else if (!regexCreditCard.test($('#cc-num').val())) {
+      console.log("Invalid Credit Card.");
+      error = "<h3>The credit card number entered is invalid. Please enter a valid credit card number.<br></h3>";
+      $('#error').css('color', 'red').show();
+    }
+    else if (!regexZip.test($('#zip').val())) {
+      console.log("Invalid Zip Code.");
+      error = "<h3>The zip code you entered is invalid. Please enter a valid 5 digit zip code.<br></h3>";
+      $('#error').css('color', 'red').show();
+    }
+    else if (!regexCvv.test($('#cvv').val())) {
+      console.log("Invalid CVV.");
+      error = "<h3>The cvv number you entered is invalid. Please enter a valid 3 digit cvv.<br></h3>";
+      $('#error').css('color', 'red').show();
+    }
+    else {
+      console.log("Success.");
+      error = "<h3>You have submitted the form successfully.<br></h3>";
+      $('#error').css('color', 'green').show();
+      $('form')[0].reset();
+    }
+
+    // inserts the error text into the id #error
+    document.getElementById('error').innerHTML = error;
+
+    // an animation that slowly scrolls up when the form is submitted
+    $('body, html').animate({ scrollTop: $('form').offset().top }, 'slow');
+  }
+  );
 });
 
-});
-
-
-// User Story #6
-// Users cannot select two activities at the same time
-// Write an if/else statement that prevents two events that occur at the same time
-// from being sleected
-
-
-
-// User Story #7
-// Total cost of selected activities is calculated and displayed below the list of activities
-// Add a total cost variable and add each item that is selected to the total
-
-
-// User Story #8
-// Make the credit card payment option the default selection
-
-
-// User Story #9
-// If user selects credit card, credit card option is displayed
-          // Hide paypal option
-          // Hide Bitcoin option
-// Else If user selects paypal, paypal option is displayed
-          // Hide credit card option
-          // Hide bitcoin option
-// Else If user selects bitcoin, bitcoin option is displayed
-          // Hide credit card option
-          // Hide paypal option
-
-
-
-
-// User Story #10
-// Form cannot be submitted (the page does not refresh when the submit button is clicked) 
-// until the following requirements have been met:
-      // 1. Name field isn’t blank.
-      // 2. Email field contains validly formatted e-mail address: 
-      // (doesn’t have to check that it's a real e-mail address, just that it's formatted 
-      // like one: dave@teamtreehouse.com, for example).
-      // 3. At least one checkbox under "Register for Activities" section must be selected.
-      // 4. If "Credit Card" is the selected payment option, the three fields accept only numbers: 
-      // a 13 to 16-digit credit card number, a 5-digit zip code, and 3-number CVV value
-
-
-// User Story # 11
-// On submission, the form provides an error indication or message for each field that requires validation:
-// 1. Name field
-// 2. Email field
-// 3. “Register for Activities” checkboxes
-// 4. Credit Card number, Zip code, and CVV, only if the credit card payment method is selected.
-
-
-
-
-// User Story #12
-// When JavaScript is disabled, all form fields and payment information is displayed, 
-// including the "Other" field under the "Job Role" section.
